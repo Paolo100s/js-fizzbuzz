c6beb1442c268eedf55c68040eb5a9d86e9ad887
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-05-03 00:31:46 +0200

    add numbers and style

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+// variabili
+const numb = document.querySelector("ul");
+
+// creazione numeri
+for (let n = 1; n <= 100; n++) {
+
+    const element =`<li class="box box__${n}">${n}</li>`;
+
+    numb.innerHTML+= element
+}
