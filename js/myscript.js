47e033d32c6ab57564056e021f3a56bddd80685f
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-05-03 16:53:05 +0200

    fizzbuzz

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,33 @@
 // variabili
-const numb = document.querySelector("ul");
+const contenitore = document.getElementById("box");
 
 // creazione numeri
-for (let n = 1; n <= 100; n++) {
+for (let n = 1; n <=100; n++) {
 
-    const element =`<li class="box box__${n}">${n}</li>`;
+    // creazione elemento
+    const quadrato = document.createElement("div")
+    quadrato.classList.add("square")
 
-    numb.innerHTML+= element
+    let numb;
+
+    // divisibile per 3 e per 5
+    if (n % 3 === 0 && n % 5 === 0) {
+        numb = "FizzBuzz"
+    }
+
+    // divisibile per 3
+    else if (n % 3 === 0)
+        numb = "Fizz"
+
+    // divisibile per 5
+    else if (n % 5 === 0)
+        numb = "Buzz"
+
+    // altro
+    else 
+       numb = n
+    
+
+    quadrato.append(numb);
+    contenitore.append(quadrato);
 }
